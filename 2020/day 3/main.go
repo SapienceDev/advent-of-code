9269c47bb464208ff67e441c5ad8c605b14a37e3
Sap ★ <samuel.newmark10@gmail.com>
2021-01-07 09:49:49 -0600

    Day 3

---
+++ 2020/day 3/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+
+	"github.com/SapienceDev/advent-of-code/utils"
+)
+
+func create2D(arr []string) [323][31]string {
+	var twoD [323][31]string
+	col := 0
+	row := 0
+	for i := 0; i < len(arr); i++ {
+		if col == 31 {
+			col = 0
+			row++
+		} else {
+			twoD[row][col] = arr[i]
+			col++
+		}
+	}
+	return twoD
+}
+
+func walk(arr [323][31]string, right int, down int) {
+	col := 0
+	trees := 0
+	for row := down; row < 323; row += down {
+		col += right
+		if col > 10 {
+			col %= 31
+		}
+		if arr[row][col] == "#" {
+			trees++
+		}
+	}
+	fmt.Printf("slope: right %d, down %d | %d\n", right, down, trees)
+}
+
+func main() {
+	data, err := ioutil.ReadFile("input.txt")
+	utils.Handle(err)
+	input := string(data)
+	arr := strings.Split(input, "")
+	twoD := create2D(arr)
+	walk(twoD, 1, 1)
+	walk(twoD, 3, 1)
+	walk(twoD, 5, 1)
+	walk(twoD, 7, 1)
+	walk(twoD, 1, 2)
+}
