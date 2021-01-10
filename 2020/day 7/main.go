09254da6683989ad55363f1ae68cc4c3819eec68
Sap ★ <samuel.newmark10@gmail.com>
2021-01-10 09:45:51 -0600

    Improved Day 7, Part 1 efficiency by ~900ms

--- 2020/day 7/main.go
+++ 2020/day 7/main.go
@@ -11,38 +11,42 @@ import (
 func main() {
 	data := utils.ReadFile("input.txt")
 	input := string(data)
-	bags := strings.Split(input, "\n")
+	arr := strings.Split(input, "\n")
 
-	var valid []string
+	bags := make(map[string]string)
 
-	// get all direct matches
-	for i, bag := range bags {
-		re := regexp.MustCompile("([a-z]+ [a-z]+ bags) .+ [1-9] shiny gold bag")
-		if re.MatchString(bag) {
-			res := re.FindAllStringSubmatch(bags[i], -1)
-			name := res[0][1]
-			valid = append(valid, fmt.Sprintf("%s?", name))
+	for i := range arr {
+		re := regexp.MustCompile("([a-z]+ [a-z]+ bags)\\scontain")
+		res := re.FindAllStringSubmatch(arr[i], -1)
+		name := res[0][1]
+		re = regexp.MustCompile("([1-9]) ([a-z]+ [a-z]+) bag")
+		res = re.FindAllStringSubmatch(arr[i], -1)
+		contains := ""
+		for _, match := range res {
+			contains += fmt.Sprintf(" %s bags", match[2])
 		}
+		bags[name] = strings.Trim(contains, " ")
 	}
 
-	// get all indirect matches
-	for {
-		var oldCount int = len(valid)
-		for i, bag := range bags {
-			re := regexp.MustCompile(fmt.Sprintf("([a-z]+ [a-z]+ [a-z]+) .+ [1-9] (%s)", strings.Join(valid, "|")))
-			if re.MatchString(bag) {
-				res := re.FindAllStringSubmatch(bags[i], -1)
-				name := res[0][1]
-				if !contains(valid, fmt.Sprintf("%s?", name)) {
-					valid = append(valid, fmt.Sprintf("%s?", name))
+	ans := find(bags, "shiny gold")
+	fmt.Printf("Part 1: %d\n", len(ans))
+}
+
+func find(bags map[string]string, s string) (res []string) {
+	for k, v := range bags {
+		if strings.Contains(v, s) {
+			res = append(res, k)
+			ans := find(bags, k)
+			if len(ans) > 0 {
+				for _, val := range ans {
+					if !contains(res, val) {
+						res = append(res, val)
+					}
 				}
 			}
 		}
-		if oldCount == len(valid) {
-			break
-		}
 	}
-	fmt.Printf("Part 1: %d", len(valid))
+	return res
 }
 
 func contains(arr []string, str string) bool {
